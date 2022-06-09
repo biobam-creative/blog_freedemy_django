56f1ddb0ca985220ec61815292e390397abcd1f1
emmanuel <eminentia.ea@gmail.com>
2022-06-09 02:14:52 +0100

    pagination setup

--- learn_with_biobam/settings.py
+++ learn_with_biobam/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('FREEDEMIA_SECRET')
 print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
