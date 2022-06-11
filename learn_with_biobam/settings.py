1c1ae009ce526234dd9f1ae6753fbea668a4a41b
emmanuel <eminentia.ea@gmail.com>
2022-06-12 00:05:41 +0100

    force to use ssl

--- learn_with_biobam/settings.py
+++ learn_with_biobam/settings.py
@@ -63,6 +63,9 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 # DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # LOGGING = {
